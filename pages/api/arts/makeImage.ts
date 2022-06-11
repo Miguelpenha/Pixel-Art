1f92ac21958d62b9bbd25b4790225cae2c32c5cf
Miguel Penha <miguel.penha1@gmail.com>
2022-06-11 09:37:08 -0300

    Ajeitando algumas coisas

--- pages/api/arts/makeImage.ts
+++ pages/api/arts/makeImage.ts
@@ -3,33 +3,34 @@ import { Iart } from '../../../types'
 import Handlebars from 'handlebars'
 import path from 'path'
 import fs from 'fs'
+import puppeteer from 'puppeteer'
+const html2canvas = require('html2canvas')
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') { 
-        if (!req.body.download) {
-            const { pixels, pixelsCont } = req.body as Iart
-  
-            const template = Handlebars.compile<{
-                title: string,
-                pixels: Iart['pixels'],
-                sizeArt: number
-            }>(fs.readFileSync(path.resolve(process.cwd(), 'public', 'views', 'makeImage.handlebars')).toString('utf-8'))
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.addScriptTag({url: 'https://html2canvas.hertzen.com/dist/html2canvas.js'})
+        await page.setRequestInterception(true)
+        const { pixels, pixelsCont } = req.body as Iart
+
+        const template = Handlebars.compile<{
+            pixels: Iart['pixels'],
+            sizeArt: number
+        }>(fs.readFileSync(path.resolve(process.cwd(), 'public', 'views', 'makeImage.handlebars')).toString('utf-8'))
+
+        await page.setContent(template({
+            pixels,
+            sizeArt: Math.sqrt(pixelsCont)
+        }))
+
+        const urlImageArt = await page.evaluate(async () => {
+            const canvas = await html2canvas(document.getElementById('art'))
             
-            res.setHeader('Content-Type', 'text/html; charset=utf-8')
-            res.write(template({
-                title: 'asd',
-                pixels,
-                sizeArt: Math.sqrt(pixelsCont)
-            }))
+            return canvas.toDataURL('image/png')
+        })
 
-            res.end()
-        } else {
-            const { download } = req.body as {
-                download: string
-            }
-
-            res.json({ download })
-        }
+        res.json({download: urlImageArt})
     } else {
         res.status(404).end()
     }
